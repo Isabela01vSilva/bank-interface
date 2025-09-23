2e6252dccf85ab7d7c68f6ea830886e3af1b77a1
Isabela Silva <isabela.vsilva@bandtec.com.br>
2025-09-23 18:26:38 -0300

    feat: estilização do header

--- src/app/container/header/header.component.ts
+++ src/app/container/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  greeting: string = '';
+
+  ngOnInit(): void {
+      const hour = new Date().getHours();
+
+      if(hour >= 5 && hour < 12) {
+        this.greeting = 'Bom Dia, ';
+      } else if(hour >= 12 && hour < 18) {
+        this.greeting = 'Boa Tarde,'
+      } else {
+        this.greeting = 'Boa Noite, '
+      }
+  }
 }
